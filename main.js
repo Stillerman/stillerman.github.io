1c9123ab8350fd0181fd9a96f446c97e41f1f1b7
Stillerman <jason.t.stillerman@gmail.com>
2019-03-09 18:08:51 -0500

    oh yes

---
+++ main.js
@@ -0,0 +1,30 @@
+import Typed from 'typed.js'
+import tippy from 'tippy.js'
+import dragElement from './drag'
+
+var typed = new Typed('.typed', {
+  // Waits 1000ms after typing "First"
+  strings: ['mkdir website', 'cd website', 'touch index.html', 'touch index.js'],
+  loop: true,
+  typeSpeed: 25,
+  backDelay: 1000,
+  backSpeed: 10,
+  smartBackspace: true,
+  cursorChar: '_'
+})
+
+tippy.setDefaults({
+  duration: 50,
+  arrow: true,
+  arrowType: 'round',
+  theme: 'tomato',
+  distance: 15
+})
+
+tippy('#tip-git', { content: 'Github' })
+tippy('#tip-play', { content: 'Play Store' })
+tippy('#tip-term', { content: 'Terminal' })
+
+document.querySelectorAll('.window').forEach(window => {
+  dragElement(window)
+})
